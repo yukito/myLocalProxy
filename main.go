2b128d6d6120861c6fad2f0a3c1aa550c7d214c0
IAC1-Yuki Ito <yukito@IAC1-yuki-ito.local>
2016-10-04 19:20:39 +0900

    has finished basic feature of localproxy

--- main.go
+++ main.go
@@ -1,55 +1,26 @@
 package main
 
 import (
-   "fmt"
    "net/http"
-   "net/http/httputil"
-   "net"
-   "time"
-   "bytes"
-   "bufio"
+   "io/ioutil"
 )
 
-func proxyHandler(conn net.Conn) {
-   defer conn.Close()
-   conn.SetReadDeadline(time.Now().Add(10 * time.Second))
-   fmt.Println("client accept!")
-   messageBuf := make([]byte, 1024)
-   messageLen, _ := conn.Read(messageBuf)
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
 
-   message := messageBuf[:messageLen]
-
-   buf := bufio.NewReader(bytes.NewReader(message))
-   fmt.Println(buf)
-   req, _ := http.ReadRequest(buf)
-   fmt.Println(req)
    client := new(http.Client)
-   req.RequestURI = ""
-   resp, _ := client.Do(req)
+   r.RequestURI = ""
+   resp, _ := client.Do(r)
 
-   defer resp.Body.Close()
-   fmt.Println(resp)
-
-   dumpResp, _ := httputil.DumpResponse(resp, true)
-
-   resp_message := string(dumpResp)
-   conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
-   conn.Write([]byte(resp_message))
+   body, _ := ioutil.ReadAll(resp.Body)
+   for key, value := range resp.Header{
+      w.Header().Set(key, value[0])
+   }
+   w.Write(body)
+   w.WriteHeader(resp.StatusCode)
 
 }
 
 func main() {
-   port := ":8888"
-   tcpAddr, _ := net.ResolveTCPAddr("tcp", port)
-   listner, _ := net.ListenTCP("tcp", tcpAddr)
-   for {
-      conn, err := listner.Accept()
-         if err != nil {
-            continue
-         }
-
-      go proxyHandler(conn)
-   }
-//   http.HandleFunc("/", proxyHandler)
-//   http.ListenAndServe("localhost:8888", nil)
+   http.HandleFunc("/", proxyHandler)
+   http.ListenAndServe("localhost:8888", nil)
 }
